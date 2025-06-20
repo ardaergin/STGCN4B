9a01575fb828c7f6fb99d9557978aeb2482660eb
ardaergin <ardergin@gmail.com>
2025-06-20 22:20:24 +0200

    Update get_data_filename.py

--- src/utils/get_data_filename.py
+++ src/utils/get_data_filename.py
@@ -9,9 +9,9 @@ def main():
     args = parse_args()
     
     if args.task_type == "measurement_forecast":
-        fname = f"torch_input_{args.adjacency_type}_{args.interval}_{args.graph_type}_{args.measurement_type}.pt"
+        fname = f"stgcn_input_{args.adjacency_type}_{args.interval}_{args.graph_type}_{args.measurement_type}.pt"
     else:
-        fname = f"torch_input_{args.adjacency_type}_{args.interval}_{args.graph_type}.pt"
+        fname = f"stgcn_input_{args.adjacency_type}_{args.interval}_{args.graph_type}.pt"
         
     print(fname)
 
